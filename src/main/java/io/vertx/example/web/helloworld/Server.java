a646bde80b6603b2fcdf77e3e0e2043614d65782
Himanshu Gupta <himanshu.gupta14@aexp.com>
2019-06-06 15:19:13 +0530

    first commit

---
+++ src/main/java/io/vertx/example/web/helloworld/Server.java
@@ -0,0 +1,35 @@
+package io.vertx.example.web.helloworld;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.example.util.Runner;
+import io.vertx.ext.web.Router;
+
+/*
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class Server extends AbstractVerticle {
+
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(Server.class);
+  }
+
+  @Override
+  public void start() throws Exception {
+
+    Router router = Router.router(vertx);
+
+    router.route().handler(routingContext -> {
+      routingContext.response().putHeader("content-type", "text/html").end("Hello World!");
+    });
+
+    vertx.createHttpServer().requestHandler(router).listen(8080, result -> {
+		if (result.succeeded()) {
+			System.out.println("Server started on port 8080");
+        } else {
+			System.err.println("Server failed to start on port 8080");
+			result.cause().printStackTrace();
+		}
+	});
+  }
+}
